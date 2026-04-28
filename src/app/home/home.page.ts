a33d481cfc378ebb8c994708430dc8d0833bbc4f
Gabriel Malavolta <gabriel.malavoltawork@gmail.com>
2026-04-27 22:00:52 -0300

    feat: list characters with grid and cards

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent,IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 import { RickMortyService } from '../core/services/rick-morty';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonGrid, IonRow, IonCol],
 })
 
 
